84aeac7f9ec1335b395960a3349797cade65f525
shari4ov <kenansharifov400@gmail.com>
2022-09-23 18:40:03 +0000

    modified:   config/db.go 	modified:   go.mod 	modified:   go.sum 	new file:   local.env

--- config/db.go
+++ config/db.go
@@ -1,14 +1,43 @@
 package config
 
-import "fmt"
+import (
+	"fmt"
+	"os"
 
-const (
-	DBUser     = "kenan"
-	DBPassword = "2001"
-	DBName     = "goecho"
-	DBHost     = "10.128.0.3"
-	DBPort     = "5432"
-	DBType     = "postgres"
+	"github.com/joho/godotenv"
+)
+
+type DB struct {
+	DBUser     string
+	DBPassword string
+	DBName     string
+	DBHost     string
+	DBPort     string
+	DBType     string
+}
+
+func returnDbENV() DB {
+	var db DB
+	err := godotenv.Load("local.env")
+	fmt.Println(err)
+	db.DBUser = os.Getenv("DBUser")
+	db.DBPassword = os.Getenv("DBPassword")
+	db.DBName = os.Getenv("DBName")
+	db.DBHost = os.Getenv("DBHost")
+	db.DBPassword = os.Getenv("DBPassword")
+	db.DBPort = os.Getenv("DBPort")
+	db.DBType = os.Getenv("DBType")
+	return db
+}
+
+var db DB = returnDbENV()
+var (
+	DBUser     = db.DBUser
+	DBPassword = db.DBPassword
+	DBName     = db.DBName
+	DBHost     = db.DBHost
+	DBPort     = db.DBPort
+	DBType     = db.DBType
 )
 
 func GetDBType() string {
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/jasonlvhit/gocron v0.0.1 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
 	github.com/labstack/echo v3.3.10+incompatible // indirect
 	github.com/labstack/echo/v4 v4.9.0 // indirect
 	github.com/labstack/gommon v0.3.1 // indirect
